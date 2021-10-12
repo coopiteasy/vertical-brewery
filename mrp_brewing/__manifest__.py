d8ef2111b3bdbaa60c4e944258a3fdeb80e32e00
robin.keunen <robin@keunen.net>
2021-10-12 17:07:31 +0200

    [MIG] mrp_brewing: fix view typo

--- mrp_brewing/__manifest__.py
+++ mrp_brewing/__manifest__.py
@@ -27,7 +27,7 @@
         "report/mrp_brewing_report.xml",
         "report/report_layout.xml",
         "wizard/stock_reports_view.xml",
-        "wizard/recompute_qty_afte_move_view.xml",
+        "wizard/recompute_qty_after_move_view.xml",
         "views/product_template_views.xml",
         "views/purchase_views.xml",
         "views/product_pricelist_views.xml",
