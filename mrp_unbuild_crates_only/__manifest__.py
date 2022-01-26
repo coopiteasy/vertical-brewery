5b4e1688a9047ef3d1e432bdc4b5e3b4960d9879
hugues de keyzer <odoo@hugues.info>
2022-01-26 14:43:37 +0100

    [ADD] new module mrp_unbuild_crates_only

---
+++ mrp_unbuild_crates_only/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Unbuild crates only",
+    "summary": "List only crates when creating an unbuild order",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "mrp_brewing",
+        "mrp_unbuild_product_mo_filter",
+    ],
+    "excludes": [],
+    "data": [
+        "views/mrp_unbuild.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
